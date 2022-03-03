9ffc0e188f16cbd88246c0e5800bf68f4fc9286b
brettwsitz <brettsit@buffalo.edu>
2022-03-03 16:55:59 -0500

    another one

---
+++ ericbish/src/client.cpp
@@ -0,0 +1,8 @@
+class Client {
+public:
+  int port_listen;
+
+  Client (int port) {
+    port_listen = port;
+  }
+}
---
+++ ericbish/src/server.cpp
@@ -0,0 +1,8 @@
+class Server {
+public:
+  int port_listen;
+
+  Server (int port) {
+    port_listen = port;
+  }
+}
